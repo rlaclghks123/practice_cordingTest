4887d9978bbcce7b514a48eb0a261023883367ff
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-13 12:24:02 +0900

    4연산 완료

--- src/bfsPractice/Main.java
+++ src/bfsPractice/Main.java
@@ -8,68 +8,56 @@ import java.util.*;
 
 public class Main {
 	
-	static int check(int num, int index, int digit) {
-		if(index== 0 && digit==0) {
-			return -1;
-		}
-		String s = Integer.toString(num);
-		StringBuilder sb = new StringBuilder(s);
-		sb.setCharAt(index, (char)(digit+'0'));
-		return Integer.parseInt(sb.toString());
-	}
+	static long limit = 1000000000L;
 	
 	public static void main(String[] args)throws IOException {
     	
 	Scanner sc = new Scanner(System.in);	
 	
-	int t =sc.nextInt();
 	
-	boolean prime [] = new boolean[10001];
+	long a =sc.nextLong();
+	long b=sc.nextLong();
 	
-	for(int i=2; i<=10000; i++) {
-		if(prime[i]==false) {
-			for(int j=i+i; j<=10000; j+=i) {
-				prime[j]=true;
+	Queue<Long> q = new LinkedList<Long>();
+    Queue<String> qs = new LinkedList<String>();
+    HashSet<Long> check = new HashSet<Long>();
+    q.add(a);
+    qs.add("");
+    check.add(a);
+	
+	while(!q.isEmpty()) {
+		long x=q.remove();
+		String str=qs.remove();
+		
+		if(x==b) {
+			if(str.length()==0) {
+				str="0";
 			}
+			System.out.println(str);
+			System.exit(0);
+		}
+		if(0<=x*x && x*x<=limit && check.contains(x*x)==false) {
+			q.add(x*x);
+			qs.add(str+"*");
+			check.add(x*x);
+		}
+		if(0<=x+x && x+x<=limit && check.contains(x+x)==false) {
+			q.add(x+x);
+			qs.add(str+"+");
+			check.add(x+x);
+		}
+		if(0<=x-x && x-x<=limit && check.contains(x-x)==false) {
+			q.add(x-x);
+			qs.add(str+"-");
+			check.add(x-x);
+		}
+		if(x!=0 && 0<=x/x && x/x<=limit && check.contains(x/x)==false) {
+			q.add(x/x);
+			qs.add(str+"/");
+			check.add(x/x);
 		}
 	}
-//	for(int i=0; i<=10000; i++) {
-//		prime[i]=!prime[i];
-//	}
-	
-	while(t-->0) {
-		int n=sc.nextInt();
-		int m=sc.nextInt();
-		
-		boolean c[] = new boolean[10001];
-		int d[] = new int[10001];
-		
-		Queue<Integer> q = new LinkedList<>();
-		
-		
-		q.add(n);
-		c[n]=true;
-		
-		while(!q.isEmpty()) {
-			int x=q.remove();
-			for(int i=0; i<4; i++)
-				for(int j=0; j<=9; j++) {
-					int temp = check(x,i,j);
-					if(temp!=-1) {
-						if(!prime[temp] && c[temp]==false) {
-							q.add(temp);
-							c[temp]=true;
-							d[temp]=d[x]+1;
-						}
-					}
-				}
-		}
-		if(d[m]==-1) {
-			System.out.println("Impossible");
-		}else {
-			System.out.println(d[m]);			
-		}
-	}
+	System.out.println(-1);
 	
    }
 }
---
+++ src/bfsPractice/four_calculation.java
@@ -0,0 +1,65 @@
+package bfsPractice;
+
+import java.util.*;
+
+public class four_calculation {
+
+	// 최소 연산을 구하는 문제 -> 숫자 - 숫자의 최단거리 BFS
+	// 범위가 큰데 어떻게 BFS? => 실제 -,/는 거의 안쓰고, +,*만 쓰는데 제곱수 이므로 숫자자체는 얼마 안큼 
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);	
+		
+		long s=sc.nextLong();		//범위가 크기때문에 long으로받는다.
+		long t=sc.nextLong();
+		
+		Queue<Long> q = new LinkedList<>();		//q 또한 long 
+		Queue<String> qs = new LinkedList<>();
+		HashSet<Long> check = new HashSet<>();	// 배열로 체크하기엔 너무 커서 HashSet사용(중복방지,크기변형)
+		Long limit = 1000000000L;				//범위 최대값 
+		
+		q.add(s);					// s부터 탐색 
+		qs.add("");					// 처음엔 "" 빈 문자열 
+		check.add(s);				//방문처리 
+		
+		while(!q.isEmpty()) {
+			long x=q.remove();	
+			String str = qs.remove();
+			
+			if(x==t) {					// 목표인 t값과 같다면 
+				if(str.length()==0) {	//str이 0일 경우 
+					str="0";			//"0"으로 표시 
+				}
+				System.out.println(str);	//str 출력 
+				System.exit(0);				//출력후 즉시 종료 
+			}
+			
+			if(0<=x*x && x*x<=limit && check.contains(x*x)==false) {
+				q.add(x*x);
+				qs.add(str+"*");
+				check.add(x*x);
+			}
+			
+			if(0<=x+x && x+x<=limit && check.contains(x+x)==false) {
+				q.add(x+x);
+				qs.add(str+"+");
+				check.add(x+x);
+			}
+			
+			if(0<=x-x && x-x<=limit && check.contains(x-x)==false) {
+				q.add(x-x);
+				qs.add(str+"-");
+				check.add(x-x);
+			}
+			
+			if(x!=0 && 0<=x/x && x/x<=limit && check.contains(x/x)==false) {	//문제에서 0이 아닐경우에만 / 가능 
+				q.add(x/x);
+				qs.add(str+"/");
+				check.add(x/x);
+			}
+		}
+		System.out.println(-1); 		//바꿀수 없는경우 -1출력 
+	}
+
+}
