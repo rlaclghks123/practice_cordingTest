320ff33dc23f87574ee738ec07c0243b950471af
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-15 14:08:25 +0900

    전구와스위치 2138 완료

--- src/gridPractice/Main.java
+++ src/gridPractice/Main.java
@@ -2,58 +2,82 @@ package gridPractice;
 
 import java.util.*;
 
-public class Main {
 
-	static void flip(int a[][], int x, int y) {
-		for(int i=x-1; i<=x+1; i++) {
-			for(int j=y-1; j<=y+1; j++) {
-				a[i][j] = 1-a[i][j];
+public class Main {
+	
+	static void change(int a[], int index) {
+		for(int i=index-1; i<=index+1; i++) {
+			if(i>=0 && i<a.length) {
+				a[i]=1-a[i];
 			}
 		}
 	}
+	
+	static Pair go(int a[], int [] goal) {
+		int n=a.length;
+		int ans=0;
+		
+		for(int i=0; i<n-1; i++) {
+			if(a[i]!=goal[i]) {
+				ans++;
+				change(a,i+1);
+			}
+		}
+		
+		boolean ok=true;
+		for(int i=0; i<n; i++) {
+			if(a[i]!=goal[i]) {
+				ok=false;
+			}
+		}
+		if(ok) {
+			return new Pair(true,ans);
+		}else {
+			return new Pair(false,ans);
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 
 		int n=sc.nextInt();
-		int m=sc.nextInt();
 		
-		int a[][] = new int[n][m];
-		int b[][] = new int[n][m];
+		int a[] = new int[n];
+		int goal[] = new int[n];
 		
+		String s = sc.next();
 		for(int i=0; i<n; i++) {
-			String s = sc.next();
-			for(int j=0; j<m; j++) {
-				a[i][j]=s.charAt(j)-'0';
-			}
+			a[i]=s.charAt(i)-'0';
 		}
 		
+		s=sc.next();
 		for(int i=0; i<n; i++) {
-			String s = sc.next();
-			for(int j=0; j<m; j++) {
-				b[i][j]=s.charAt(j)-'0';
-			}
-		}
-		int ans=0;
-		
-		for(int i=0; i<n-2; i++) {
-			for(int j=0; j<m-2; j++) {
-				if(a[i][j]!=b[i][j]) {
-					ans++;
-					flip(a,i+1,j+1);
-				}
-			}
+			goal[i]=s.charAt(i)-'0';
 		}
 		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<m; j++) {
-				if(a[i][j]!=b[i][j]) {
-					System.out.println(-1);
-					System.exit(0);
-				}
-			}
+		int b[] = new int[a.length];
+		System.arraycopy(a, 0, b, 0, a.length);
+		
+		Pair p1=go(b,goal);
+		
+		change(a,0);
+		Pair p2=go(a,goal);
+		
+		if(p2.first) {
+			p2.second++;
 		}
-		System.out.println(ans);
+		
+		if(p1.first && p2.first) {
+			System.out.println(Math.min(p2.second, p1.second));
+		}else if(p1.first) {
+			System.out.println(p1.second);
+		}
+		else if(p2.first) {
+			System.out.println(p2.second);
+		}else {
+			System.out.println(-1);
+		}
 		}
 }
