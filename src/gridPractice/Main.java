af5cce30b56443df72c391c407c407591db8f398
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-19 09:54:38 +0900

    재귀 패키지 생성 및 알파벳1987문제 완

--- src/gridPractice/Main.java
+++ src/gridPractice/Main.java
@@ -1,48 +1,105 @@
 package gridPractice;
 
+import java.io.*;
 import java.util.*;
-
 
 public class Main {
 	
-	static char flip(char temp) {
-		if(temp=='H') {
-			return 'T';
-		}else {
-			return 'H';
-		}
-	}
-	public static void main(String[] args) {
+	static int dx[] = {1,-1,0,0};
+	static int dy[] = {0,0,1,-1};
+	static int group[][];
+	static boolean c[][];
+	static int a[][];
+	static int n,m;
+	static ArrayList<Integer> group_size = new ArrayList<>();
+	
+	static void bfs(int sx, int sy) {
 		
-		Scanner sc = new Scanner(System.in);
-
-		int n=sc.nextInt();
+		int g=group_size.size();
+		int cnt=1;
 		
-		String a[] = new String[n];
+		Queue<Integer> q = new LinkedList<>();
+		q.add(sx); q.add(sy);
+		group[sx][sy]=g;
+		c[sx][sy]=true;
 		
-		for(int i=0; i<n; i++) {
-			a[i]=sc.next();
-		}
-		
-		int ans=n*n;
-		
-		for(int col=0; col<(1<<n); col++) {
-			int sum=0;
-			for(int j=0; j<n; j++) {
-				int cnt=0;
-				for(int i=0; i<n; i++) {
-					char temp = a[i].charAt(j);
-					if((col&(1<<i))!=0) {
-						temp=flip(temp);
-					}
-					if(temp=='T') {
-						cnt++;
+		while(!q.isEmpty()) {
+			int x=q.remove();
+			int y=q.remove();
+			for(int i=0; i<4; i++) {
+				int nx=x+dx[i];
+				int ny=y+dy[i];
+				if(0<=nx && nx<n && ny>=0 && ny<m) {
+					if(a[nx][ny]==0 && c[nx][ny]==false) {
+					q.add(nx); q.add(ny);
+					group[nx][ny]=g;
+					c[nx][ny]=true;
+					cnt++;
 					}
 				}
-				sum+=Math.min(cnt, n-cnt);
 			}
-			if(ans>sum) ans=sum;
 		}
-		System.out.println(ans);
-		}
+		
+	group_size.add(cnt);	
+	}
+    
+    public static void main(String args[]) throws IOException {
+   
+//    	Scanner sc  = new Scanner(System.in);
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	String s[] = br.readLine().split(" ");
+    	n = Integer.parseInt(s[0]);
+    	m = Integer.parseInt(s[1]);
+    	
+    	a = new int[n][m];
+    	group = new int[n][m];
+    	c = new boolean [n][m];
+    	
+    	for(int i=0; i<n; i++) {
+    		String str=br.readLine();
+    		for(int j=0; j<m; j++) {
+    			a[i][j]=str.charAt(j)-'0';
+    			group[i][j]=-1;
+    			c[i][j]=false;
+    		}
+    	}
+    	
+    	for(int i=0; i<n; i++) {
+    		for(int j=0; j<m; j++) {
+    			if(a[i][j]==0 && c[i][j]==false) {
+    				bfs(i,j);
+    			}
+    		}
+    	}
+    	
+    	for(int i=0; i<n; i++) {
+    		for(int j=0; j<m; j++) {
+    			if(a[i][j]==0) {
+    				bw.write("0");
+    			}else {
+    				HashSet<Integer> near = new HashSet<>();
+    				for(int k=0; k<4; k++) {
+    					int x=i+dx[k];
+    					int y=j+dy[k];
+    					if(0<=x && x<n && y>=0 && y<m) {
+    						if(a[x][y]==0) {
+    							near.add(group[x][y]);
+    						}
+    					}
+    				}
+    				int ans=1;
+    				for(int g:near) {
+    					ans+=group_size.get(g);
+    				}
+    				bw.write(String.valueOf(ans%10));
+    			}
+    		}
+    		bw.write("\n");
+    	}
+    	bw.flush();
+    	
+    }
 }
