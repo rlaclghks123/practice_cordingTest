cc378f7dfa9ac36c038829b3a07772a7829ba7ec
Chi Hwan Kim <rlaclghks123@naver.com>
2022-08-05 22:37:47 +0900

    폰캔몬 완

--- programmers/StringPractice/Main.java
+++ programmers/StringPractice/Main.java
@@ -8,8 +8,17 @@ public class Main {
     public String solution(String new_id) {
         String answer = "";
         answer = new_id.toLowerCase();
-        answer = answer.replaceAll("^[0-9a-z-_.]", "");
-        System.out.print(answer+" ");
+        answer = answer.replaceAll("[^0-9a-z-_.]", "");
+        answer=answer.replaceAll("\\.+", ".");
+        answer=answer.replaceAll("^[.]|[.]$", ".");
+        answer = (answer.isEmpty() ? "a" : answer);
+        if(answer.length()>15) {
+        	answer=answer.substring(0, 15);
+        	answer=answer.replaceAll("[.]$", "");
+        }
+        while(answer.length()<3) {
+        	answer+=answer.charAt(answer.length()-1);
+        }
         return answer;
     }
 }
--- programmers/hash/phoneCatMon.java
+++ programmers/hash/phoneCatMon.java
@@ -1,5 +1,25 @@
 package hash;
 
-public class phoneCatMon {
+import java.util.*;
 
+public class phoneCatMon {
+	class Solution {
+	    public int solution(int[] nums) {
+	        int answer = 0;
+	        int max=nums.length/2;		//문제에서 n/2마리의 폰켄몬을 가져갈수 있기때문 
+	        	
+	        HashSet<Integer> hashSet = new HashSet<>();	//중복을 제거하기 위해 HashSet사용 
+	        
+	        for(int num:nums) {			//입력배열을 하나씩 빼서 
+	        	hashSet.add(num);		//hashSet에 담아준다. (중복제거)
+	        }
+	        
+	        if(max<hashSet.size()) {		//최대값보다 큰경우 
+	        	answer=max;					//문제최대값인 max값 출력 
+	        }else {
+	        	answer=hashSet.size();		//최대값보다 적은경우 중복제거된 개수 출력 
+	        }
+	        return answer;
+	    }
+	}
 }
