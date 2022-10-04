a914bdb407e25f98c75f2e56c06f5b06787a9b3d
chiman <rlaclghks123@naver.com>
2022-10-04 10:40:10 +0900

    그대로출력하기 11729 완료

---
+++ baekjoon/문자열/그대로출력하기11729.java
@@ -0,0 +1,19 @@
+package 문자열;
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class 그대로출력하기11729 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {							//입력이 존재하면 계속 받는다.
+			System.out.println(sc.nextLine());			//nextLine을 통해 한줄전체를 입력받아 출력한다.
+		}
+		
+	}
+}
