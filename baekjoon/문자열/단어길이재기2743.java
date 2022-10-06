4756c0cb162a00d9a5397a4e5beee25f4e68534b
chiman <rlaclghks123@naver.com>
2022-10-06 15:56:26 +0900

    단어길이재기 2743 완료

---
+++ baekjoon/문자열/단어길이재기2743.java
@@ -0,0 +1,16 @@
+package 문자열;
+
+import java.io.*;
+import java.util.*;
+
+public class 단어길이재기2743 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+			
+		String s = sc.nextLine();				//문자열 입력받는다.
+		
+		System.out.println(s.length());			//길이를 출력 
+	}
+}
